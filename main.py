3ed88a0a96f64eec0449056a38d203b133a39716
MosesPitchai <96627495+MosesPitchai@users.noreply.github.com>
2025-12-05 04:15:20 +0530

    Fix indentation of resource_path function
    Indent the resource_path function for proper formatting.

--- main.py
+++ main.py
@@ -146,10 +146,10 @@ def main(page: ft.Page):
 
         page.update()
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    if hasattr(sys, '_MEIPASS'):
-        return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
+    def resource_path(relative_path):
+        """ Get absolute path to resource, works for dev and for PyInstaller """
+        if hasattr(sys, '_MEIPASS'):
+            return os.path.join(sys._MEIPASS, relative_path)
+        return os.path.join(os.path.abspath("."), relative_path)
 
 ft.app(target=main)
